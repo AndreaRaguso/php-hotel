0c7d34ff00b42773908767526f0bf5ef2e24b2ac
Andrea Raguso <info@andrearaguso.com>
2023-02-20 14:18:10 +0100

    stile aggiunto

--- index.php
+++ index.php
@@ -42,9 +42,6 @@ $hotels = [
 ];
 
 
-
-
-
 ?>
 
 
@@ -53,31 +50,43 @@ $hotels = [
 
 
 <!DOCTYPE html>
-    <html lang="it">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Php Hotel</title>
-    </head>
-    <body>
-        
-    
-    <?php
+<html lang="it">
 
-    foreach($hotels as $key => $hotel){
-        echo '<h1> Hotel numero '. $key+1 .'</h1>';
-        echo '<ul>';
-        echo '<li>'.$hotel['name'].'</li>';
-        echo '<li>'.$hotel['description'].'</li>';
-        echo '<li>'.$hotel['parking'].'</li>';
-        echo '<li>'.$hotel['vote'].'</li>';
-        echo '<li>'.$hotel['distance_to_center'].'</li>';
-        echo '</ul>';
-    }
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Php Hotel</title>
+</head>
 
-    ?>
+<body>
+
+    <h1 class="text-center my-5">Hotel Disponibili a Lecce</h1>
+
+    <div class="container-lg">
+        <div class="row">
+
+            <?php
+            foreach ($hotels as $key => $hotel) {
+                echo '<div class="col-auto card mx-auto mb-3 hotel-card py-4">';
+                echo '<h2 class="text-center mb-4"> Hotel numero ' . $key + 1 . '</h2>';
+                echo '<ul>';
+                echo '<li><strong>Nome: </strong>' . $hotel['name'] . '</li>';
+                echo '<li><strong>Descrizione: </strong>' . $hotel['description'] . '</li>';
+                echo '<li><strong>Parcheggio: </strong>'. ($hotel['parking'] ? 'Presente' : 'Assente' )  .'</li>';
+                echo '<li><strong>Voto: </strong>' . $hotel['vote'] . ' stelle</li>';
+                echo '<li><strong>Distanza dal centro : </strong>' . $hotel['distance_to_center'] . ' metri</li>';
+                echo '</ul>';
+                echo '</div>';
+            }
+            ?>
+
+        </div>
+    </div>
 
 
-    </body>
+</body>
+
 </html>
